d9066d16defd603b327731949282b054fc4bbafd
Eric Apostal <ehapostal04@gmail.com>
2025-12-24 14:15:23 -0500

    better dependency chain

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:cubestrap/features/minecraft/repositories/authentication.dart';
-import 'package:cubestrap/features/xbox/repositories/xbox_client.dart';
 import 'package:flutter/material.dart';
-import 'package:modrinth_dart_api/modrinth_dart_api.dart';
+import 'package:modrinth/modrinth.dart';
 
 void main() {
   runApp(const Cubestrap());
